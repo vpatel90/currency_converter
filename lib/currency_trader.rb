c01fd15d0716b5143e2349271a26048c12af2cb5
Vivek Patel <vnp229@gmail.com>
2016-03-18 15:16:42 -0500

    currency trader object initialized

---
+++ lib/currency_trader.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/currency_converter'
+
+class CurrencyTrader
+  def initialize(starting_currency, converters)
+    @starting_currency = starting_currency
+    @converters = converters
+  end
+
+  def trade
+    
+  end
+
+end
---
+++ test/currency_trader_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/currency_trader'
+
+class CurrencyTraderTest < Minitest::Test
+  def setup
+    @starting_currency = Currency.new('$1000')
+    @converters = [CurrencyConverter.new({USD:1.0,EUR:0.88346,GBP:0.69075}),
+                  CurrencyConverter.new({USD:1.0,EUR:0.88346,GBP:0.89075})]
+
+  end
+
+  def test_currency_trader
+    assert CurrencyTrader.new(@starting_currency, @converters)
+  end
+
+
+end
