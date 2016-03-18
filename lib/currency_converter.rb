0be684ee05142b154ccc1500758a0a9bcba6795d
Vivek Patel <vnp229@gmail.com>
2016-03-17 19:32:16 -0500

    convet method complete

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -1,8 +1,21 @@
 require_relative '../lib/currency'
+class UnknownCurrencyError < StandardError
+  def message
+    "The currency is unknown to the converter"
+  end
+end
 
 class CurrencyConverter
-  attr_accessor :conversion_rate
+  attr_accessor :rate
   def initialize(conversion_rate)
-    @conversion_rate = conversion_rate
+    @rate = conversion_rate
+  end
+
+  def convert(currency, new_code)
+    raise UnknownCurrencyError if @rate[currency.curr_code.to_sym].nil? || @rate[new_code.to_sym].nil?
+
+    new_value = (@rate[new_code.to_sym] /
+                @rate[currency.curr_code.to_sym]) * currency.amount
+    Currency.new(new_value.round(2), new_code)
   end
 end
--- test/currency_converter_test.rb
+++ test/currency_converter_test.rb
@@ -5,15 +5,30 @@ require_relative '../lib/currency_converter'
 
 class CurrencyConverterTest < Minitest::Test
   def test_currency_converter
-    assert CurrencyConverter.new
+    assert CurrencyConverter.new({USD:1.0,EUR:0.88346,GBP:0.69075})
+  end
+
+  def setup
+    @converter = CurrencyConverter.new( {USD:1.0,EUR:0.88346,GBP:0.69075} )
   end
 
   def test_conversaion_rate_exists
-    converter = CurrencyConverter.new( {USD:1.0,EUR:0.88,GBP:0.69} )
-    assert_equal({USD:1.0,EUR:0.88,GBP:0.69}, converter.conversion_rate)
+    assert_equal({USD:1.0,EUR:0.88346,GBP:0.69075}, @converter.rate)
   end
 
   def test_convert
+    currency1 = Currency.new(10, 'USD')
+    currency2 = Currency.new(8.83, 'EUR')
+    currency3 = Currency.new(14, 'USD')
+    currency4 = Currency.new(1557.71, 'Yen')
+
+    assert_equal currency2, @converter.convert(currency1, 'EUR')
+    assert_raises UnknownCurrencyError do
+      @converter.convert(currency3, 'Yen')
+    end
+    assert_raises UnknownCurrencyError do
+      @converter.convert(currency4, 'USD')
+    end
 
   end
 
